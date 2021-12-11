ab18a20fced0abfbfaed0200338c50fb03e55504
wpo9nine <wpo9nine@gmail.com>
2021-12-11 22:29:36 +0700

    add endpoint fastapi

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import fastapi
+import fastapi, json
 from fastapi.responses import FileResponse #aiofiles include
 
 app = fastapi.FastAPI()
 
-@app.get("/")
-async def earthquake_cluster_analysis():
-    return FileResponse("sumbawaEarthquake.png")
+with open("listOfRegion.json","r") as read_file:
+	data = json.load(read_file)
+
+@app.get("/region/")
+async def most_frequent_earthquake_region_indonesia():
+    return data
+
+@app.get("/region/{region_id}/")
+async def earthquake_cluster_analysis(region_id : int):
+    if region_id == 1:
+        return FileResponse("sumbawaEarthquake.png") #tinggal nambahin data selanjutnya
+    else:
+        raise fastapi.HTTPException(
+			status_code = 404,
+			detail = f'region not found. id range from 1 to 3'
+		)
